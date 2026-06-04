1379240992c20f13e6ada00b5c51e344f4da8650
KoenDR06 <koen.de.ruiter@hotmail.com>
2026-06-04 22:10:42 +0200

    rewrite

--- modules/default.nix
+++ modules/default.nix
@@ -1,15 +1,63 @@
-{lib, ...}: let
-  inherit (lib) mkOption types mkEnableOption;
+{lib, config, ...}: let
+  inherit (lib) mkOption types mapAttrsToList filterAttrsRecursive mapAttrs filterAttrs boolToString;
+  inherit (builtins) concatStringsSep typeOf;
+
+  cfg = config.hypr.config;
+
+  toStringCustom = it: if (typeOf it == "bool") then (boolToString it) else (toString it);
 in {
   imports = [
     ./options
-    ./dots
   ];
 
   options = {
-    nix-hyprland = {
-      username = mkOption {type = types.str;};
-      enable = mkEnableOption "Enable the dotfiles";
+    hypr.config = {
+      general = {
+        border_size = mkOption {
+          type = with types; nullOr int;
+          default = null;
+        };
+        gaps_in = mkOption {
+          type = with types; nullOr int;
+          default = 3;
+        };
+      };
+
+      decoration = {
+        rounding = mkOption {
+          type = with types; nullOr int;
+          default = null;
+        };
+
+        blur = {
+          enabled = mkOption {
+            type = with types; nullOr bool;
+            default = true;
+          };
+        };
+      };
     };
+
+    hypr.result = mkOption {
+      type = types.attrs;
+      readOnly = true;
+    };
+  };
+
+  config = let
+    removeNulls = filterAttrsRecursive (_: it: it != null);
+    removeEmptySets = config: let
+      res = mapAttrs (n: v: removeEmptySets v) config;
+    in if typeOf config == "set" then filterAttrs (n: v: v != {}) res else config;
+    minimizeCategory = config: removeEmptySets (removeNulls config);
+
+    catComma = concatStringsSep ",";
+    categoryToLua = config: "{" + catComma (mapAttrsToList (name: value: "${name}=" + (if (typeOf value == "set") then (categoryToLua value) else (toStringCustom value))) config) + "}";
+  in {
+    environment.etc."hyprland.lua".text = ''
+      hl.config(${categoryToLua (minimizeCategory cfg)})
+    '';
+
+    hypr.result = minimizeCategory cfg;
   };
 }
